adf418e9a0f71a827f7cfb820ebc6523bf27a3f0
Rony Kordahi <kordrony@gmail.com>
2022-01-30 18:22:11 -0500

    updated to discord.js

---
+++ discordIO.js
@@ -0,0 +1,40 @@
+const Discord = require('discord.io');
+const logger = require('winston');
+const {responses} = require("./responses");
+require("dotenv").config();
+
+// Initialize Discord Bot
+const bot = new Discord.Client({
+    token: process.env.token,
+    autorun: true
+});
+
+// Configure logger settings
+logger.remove(logger.transports.Console);
+logger.add(new logger.transports.Console, {
+    colorize: true
+});
+
+logger.level = 'debug';
+
+// confirms connection
+bot.on('ready', (evt) => {
+    logger.info('Connected');
+    logger.info('Logged in as: ');
+    logger.info(bot.username + ' - (' + bot.id + ')');
+});
+
+// event listener for messages, checks for tableflip
+bot.on('message', (user, userID, channelID, message, evt) => {
+
+    if ((message.includes("┻") || message.includes("︵")) && userID !== "85534406369894400" && userID !== "937124780799324182") {
+        
+        let response = responses[Math.floor(Math.random() * responses.length)];
+        response = response.replace("USERID", `${userID}`);
+
+        bot.sendMessage({
+            to: channelID,
+            message: response
+        })
+    }
+});
